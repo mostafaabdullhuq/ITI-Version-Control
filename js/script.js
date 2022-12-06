74cbd2c06894444a88045d9b92a9026f97ea6576
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2022-12-06 18:41:36 +0200

    add js comments

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
+// when window loads
 window.onload = function () {
+    // get form and add an event to it, when form submitted
     document.getElementById("login-form").addEventListener("submit", (e) => {
+        // prevent the form submit default behavior
         e.preventDefault();
+
+        // change form parent background color
         e.target.parentElement.style.backgroundColor = "#EEE";
     });
 };
